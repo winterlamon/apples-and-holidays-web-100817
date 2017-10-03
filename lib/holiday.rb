6101643853cbbb8a1345b55e1ec4f56279713ca0
Winter LaMon <winter.lamon@gmail.com>
2017-10-03 01:17:42 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -52,7 +52,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.to_s.capitalize}:"
     holiday.collect do |holiday, supplies|
       supplies.join(" ").capitalize!
-      supplies.capitalize!.to_a
+      supplies.capitalize.to_a
       puts "  #{holiday.to_s.capitalize}: #{supplies.join(" ").capitalize!}"
 #      puts "supplies.join(" ").capitalize!"
 #      puts supplies.split(", ")
