19ea9cde34d735d359bef6b57308e9f3033c224f
Winter LaMon <winter.lamon@gmail.com>
2017-10-03 02:03:21 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,9 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  bbq_holidays = []
   holiday_hash.collect do |season, holiday|
+    holiday.collect do |supply|
     if season.values.contains?("BBQ") do
       holiday
     end
